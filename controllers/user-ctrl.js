69a9b2d419904effc6cbe7745db798af60531a70
jkniaziii <jabbaswork@gmail.com>
2023-05-08 00:55:01 +0500

    YES

--- controllers/user-ctrl.js
+++ controllers/user-ctrl.js
@@ -1,67 +1,68 @@
 const User = require('../models/user-model')
 
-createUser = (req, res) => {
-    const body = req.body
-    const user = new User(body)
+createUser = async (req, res) => {
+  const body = req.body
+  const user = new User(body)
+  const existingUser = await User.findOne({ userId: user.userId });
 
-    if (!user) {
-        return res.status(400).json({ success: false, error: err })
+    if (existingUser) {
+      return res.status(200).json({ success: true, message: "User already exists" })
     }
-
-    user
-        .save()
-        .then(() => {
-            return res.status(200).json({
-                success: true,
-                id: user._id,
-                message: 'User created!',
-            })
+    user.save()
+      .then(() => {
+        return res.status(200).json({
+          success: true,
+          id: user._id,
+          message: 'User created!',
         })
-        .catch(error => {
-            return res.status(400).json({
-                error,
-                message: 'User not created!',
-            })
+      })
+      .catch(error => {
+        return res.status(400).json({
+          error,
+          message: 'User not created!',
         })
+  })
 }
 
+
 updateUser = async (req, res) => {
-    try {
-      const body = req.body;
-      const user = await User.findOne({ _id: req.params.id });
-  
-      if (!user) {
-        return res.status(404).json({
-          message: "User not found!",
-        });
-      }
-  
-      user.name = body.name;
-      user.email = body.email;
-      user.userId = body.userId;
-      user.isVarified = body.isVarified;
-      user.isSubscriber = body.isSubscriber;
-      user.professionalInfo = body.professionalInfo;
-  
-      await user.save();
-  
-      return res.status(200).json({
-        success: true,
-        id: user._id,
-        message: "User updated!",
-      });
-    } catch (err) {
+  try {
+    const body = req.body;
+    const user = await User.findOne({ userId: req.params.id });
+
+    if (!user) {
       return res.status(404).json({
-        error: err,
-        message: "User not updated!",
+        message: "User not found!",
       });
     }
-  };  
+
+    // Only update the fields that come from the request body
+    Object.keys(body).forEach((key) => {
+        user[key] = body[key];
+      
+    });
+
+    console.log(user);
+    await user.save();
+
+    return res.status(200).json({
+      success: true,
+      id: user._id,
+      message: "User updated!",
+    });
+  } catch (err) {
+    return res.status(404).json({
+      error: err,
+      message: "User not updated!",
+    });
+  }
+};
+
 
 getUserById = async (req, res) => {
     try {
       console.log("HERE");
-      const user = await User.findOne({ _id: req.params.id });
+      const user = await User.findOne({ userId: req.params.id });
       return res.status(200).json({ success: true, data: user });
     } catch (err) {
       return res.status(400).json({ success: false, error: err });
--- models/user-model.js
+++ models/user-model.js
@@ -3,19 +3,24 @@ const Schema = mongoose.Schema
 
 const User = new Schema(
     {
-        name: { type: String, required: true },
-        email: { type: String, required: true },
-        userId: { type: String, required: true },
-        isVarified:  { type: Boolean, required: true },
-        isSubscriber:  { type: Boolean, required: true },
+        name: { type: String },
+        email: { type: String },
+        userId: { type: String },
+        isVarified: { type: Boolean },
+        isSubscriber: { type: Boolean },
         professionalInfo: {
-            age: { type: Number },
+            age: { type: String },
             gender: { type: String },
             position: { type: String },
             industry: { type: String },
             location: { type: String },
-            experience: { type: Number },
-            job_type: { type: String }
+            experience: { type: String },
+            nameProfessional: { type: String },
+            country: { type: String },
+            number: { type: String },
+            gender: { type: String },
+            job_type: { type: String },
+            cv: { type: String }
         }
 
     },
